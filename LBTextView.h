529d3aa8d96e037206799b0a20b854310bf4e017
Dafucai <Dafucai@DafucaideMacBook-Air.local>
2018-11-12 13:59:15 +0800

    提交LBTextView

---
+++ LBTextView.h
@@ -0,0 +1,20 @@
+//
+//  LBTextView.h
+//  易训网
+//
+//  Created by Dafucai on 2018/11/12.
+//  Copyright © 2018年 com.populook.edu.gozhixin.mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LBTextView : UITextView
+
+@property (nonatomic, copy) NSString *placeholder;
+@property (nonatomic, strong) UIColor *placeholderColor;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LBTextView.m
@@ -0,0 +1,96 @@
+//
+//  LBTextView.m
+//  易训网
+//
+//  Created by Dafucai on 2018/11/12.
+//  Copyright © 2018年 com.populook.edu.gozhixin.mobile. All rights reserved.
+//
+
+#import "LBTextView.h"
+
+@interface LBTextView()
+
+@property (nonatomic, weak) UILabel *placeholderLabel;
+
+@end
+
+@implementation LBTextView
+
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // 1.添加提示文字
+        UILabel *placeholderLabel = [[UILabel alloc] init];
+        placeholderLabel.textColor = [UIColor darkGrayColor];
+        placeholderLabel.hidden = YES;
+        placeholderLabel.numberOfLines = 0;
+        placeholderLabel.backgroundColor = [UIColor clearColor];
+        placeholderLabel.font = self.font;
+        [self insertSubview:placeholderLabel atIndex:0];
+        self.placeholderLabel = placeholderLabel;
+        
+        // 2.监听textView文字改变的通知
+        [ [NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textDidChange) name:UITextViewTextDidChangeNotification object:self];
+    }
+    return self;
+}
+
+- (void)setPlaceholder:(NSString *)placeholder
+{
+    _placeholder = [placeholder copy];
+    
+    self.placeholderLabel.text = placeholder;
+    if (placeholder.length) { // 需要显示
+        self.placeholderLabel.hidden = NO;
+        
+        // 计算frame
+        CGFloat placeholderX = 10;
+        CGFloat placeholderY = 7;
+        CGFloat maxW = self.frame.size.width - 2 * placeholderX;
+        CGFloat maxH = self.frame.size.height - 2 * placeholderY;
+        CGSize placeholderSize = [placeholder sizeWithFont:self.placeholderLabel.font constrainedToSize:CGSizeMake(maxW, maxH)];
+        self.placeholderLabel.frame = CGRectMake(placeholderX, placeholderY, placeholderSize.width, placeholderSize.height);
+    } else {
+        self.placeholderLabel.hidden = YES;
+    }
+    
+}
+
+- (void)setPlaceholderColor:(UIColor *)placeholderColor
+{
+    _placeholderColor = placeholderColor;
+    
+    self.placeholderLabel.textColor = placeholderColor;
+}
+
+- (void)setFont:(UIFont *)font
+{
+    [super setFont:font];
+    
+    self.placeholderLabel.font = font;
+    self.placeholder = self.placeholder;
+}
+
+- (void)textDidChange
+{
+    self.placeholderLabel.hidden = (self.text.length != 0);
+}
+
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
